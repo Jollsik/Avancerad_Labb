9f470b7ea8d769ea3d2c5ed13afc2708c6caaf62
Joel Felldin <joelfelldin@hotmail.com>
2020-04-20 10:18:41 +0200

    Add project files.

---
+++ Avancerad_Labb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Avancerad_Labb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
