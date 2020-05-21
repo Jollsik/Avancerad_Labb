b054d75df9cbbfecf1322a21fd5f4e363bf84f59
Joel Felldin <joelfelldin@hotmail.com>
2020-05-21 11:28:13 +0200

    add-Order_Model

---
+++ Avancerad_Labb/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Avancerad_Labb.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public List<Product> products { get; set; }
+        public DateTime Date { get; set; }
+        public Guid UserId { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
