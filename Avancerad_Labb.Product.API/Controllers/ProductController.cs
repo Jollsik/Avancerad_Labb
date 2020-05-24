8ac7d7a7fbcb1451d96470f471ac481112d603ac
Joel Felldin <joelfelldin@hotmail.com>
2020-05-24 12:07:17 +0200

    Add-Initializer-for-ProductDatabase

---
+++ Avancerad_Labb.Product.API/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Avancerad_Labb.Product.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avancerad_Labb.Product.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDbContext _context;
+        public ProductController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
