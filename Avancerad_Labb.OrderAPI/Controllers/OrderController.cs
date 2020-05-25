c8cfc988b4a42a541620990f8c6ea46f9a7a1869
Joel Felldin <joelfelldin@hotmail.com>
2020-05-25 21:42:41 +0200

    add-orderDB-with-GetAndPostFunctions

---
+++ Avancerad_Labb.OrderAPI/Controllers/OrderController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Avancerad_Labb.OrderAPI.Data;
+using Avancerad_Labb.OrderAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avancerad_Labb.OrderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+        private readonly OrderDbContext _context;
+        public OrderController(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            _context.Add(order);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(PostOrder),
+                new { Id = order.Id },
+                order);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Order>> GetOrderById(Guid Id)
+        {
+            Order order = await _context.Orders.FindAsync(Id);
+            if (order != null)
+            {
+                return order;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Avancerad_Labb.OrderAPI/Data/OrderDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Avancerad_Labb.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Avancerad_Labb.OrderAPI.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options)
+    : base(options)
+        {
+        }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderRow> OrderRows { get; set; }
+    }
+}
